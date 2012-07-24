b1d7a7beaab0b3fe22ffac7088efabd9a4d8d617
Ben Langfeld <ben@langfeld.me>
2012-07-25 00:16:21 +0200

    Better packaging

---
+++ README.md
@@ -0,0 +1,18 @@
+# Internet Time
+
+  Swatch Internet Time extension to Ruby Time class.
+
+## Installation
+  $ gem install internet_time
+
+## Usage
+```
+require 'internet_time'
+Time.now.to_beats # => "@234"
+```
+
+## Author
+  Paul Philippov <themactep@gmail.com>
+
+## License:
+  New BSD License
--- lib/internet_time.rb
+++ lib/internet_time.rb
@@ -1,28 +1,21 @@
-#! /usr/bin/env ruby
-#--
-# Swatch Internet Time extension to Ruby Time class
-# Copyright 2009 Paul Philippov <themactep@gmail.com>
-#
-# This program is free software. It may be redistributed and/or modified
-# under the terms of the New BSD licence.
-#
-# $Id: beats.rb,v 1.0.2 2010/04/07 05:03:03 themactep Exp $
-#++
-
 # The library add Swatch Internet Time capability to the standard Ruby Time class.
 # Its usage is very straightforward:
 #
 #   require 'internet_time'
 #   Time.now.to_beats  # => @234
 #
-class Time #:nodoc:#
+require 'internet_time/version'
 
+module InternetTime
+end
+
+class Time
   SECOND_PER_BEAT = 86.4  # Amount of seconds in a single .beat
   BMT_OFFSET_SEC = 3600   # Biel Mean Time offset to UTC in seconds
 
   # Time in Biel Mean Time (UTC+1)
   def bmt
-    self.utc + BMT_OFFSET_SEC
+    utc + BMT_OFFSET_SEC
   end
 
   # Time in .beats
@@ -31,5 +24,3 @@ class Time #:nodoc:#
     "@%03d" % [ ((time.hour * 60 + time.min) * 60 + time.sec) / SECOND_PER_BEAT ]
   end
 end
-
-# vim: ts=2
